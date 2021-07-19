ddf2af75b74d17762328878339e996aa9a8a739c
Conespoxs <87117498+Neeacconl@users.noreply.github.com>
2021-07-19 20:31:50 +0530

    Update disable.py

--- SaitamaRobot/modules/disable.py
+++ SaitamaRobot/modules/disable.py
@@ -348,7 +348,7 @@ if is_module_loaded(FILENAME):
     • `/listcmds`*:* list all possible toggleable commands
     """
 
-    __mod_name__ = "Disable"
+    __mod_name__ = "Disable 🐶"
 
 else:
     DisableAbleCommandHandler = CommandHandler
