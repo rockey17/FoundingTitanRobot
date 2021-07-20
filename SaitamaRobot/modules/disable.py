0688d4979f64fff2660d9a06f3e8287ec3f122bf
rockey17 <81315057+rockey17@users.noreply.github.com>
2021-07-20 10:08:50 +0530

    Update disable.py

--- SaitamaRobot/modules/disable.py
+++ SaitamaRobot/modules/disable.py
@@ -337,7 +337,7 @@ if is_module_loaded(FILENAME):
     dispatcher.add_handler(COMMANDS_HANDLER)
     dispatcher.add_handler(TOGGLE_HANDLER)
 
-    __help__ = """
+    __helpee__ = """
     • `/cmds`*:* check the current status of disabled commands
 
     *Admins only:*
